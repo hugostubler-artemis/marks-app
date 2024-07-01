f121a9320f540d9632ac381f02a75e393cd73574
hugostubler-artemis <hugo.stubler@artemistechnologies.co.uk>
2024-07-01 12:44:20 -0400

    Update app_v5.py

--- app_v5.py
+++ app_v5.py
@@ -108,9 +108,9 @@ def main():
     #    st.write(location)
     
     if st.checkbox("Check my location"):
-    loc = get_geolocation()
-    st.write(f"Your coordinates are {loc}")
-    
+        loc = get_geolocation()
+        st.write(f"Your coordinates are {loc}")
+        
 
     # Dropdown menu for selecting input method
     input_method = st.sidebar.selectbox(
